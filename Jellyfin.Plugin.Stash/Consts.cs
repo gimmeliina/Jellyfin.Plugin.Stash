e038cf2df175f46016fe35056e4232267e1d9695
gimmeliina <96843881+gimmeliina@users.noreply.github.com>
2025-05-26 16:57:36 +0300

    Update Consts.cs

--- Jellyfin.Plugin.Stash/Consts.cs
+++ Jellyfin.Plugin.Stash/Consts.cs
@@ -2,10 +2,7 @@ namespace Stash
 {
     public static class Consts
     {
-        public const string SceneSearchQuery = @"query{{findScenes({0}){{scenes{{id,title,date,paths{{screenshot}}}}}}}}";
-
-        public const string SceneQuery = @"query{{findScene(id:{0}){{id,title,details,date,paths{{screenshot}}studio{{name,parent_studio{{name,image_path}},image_path}}tags{{name}}performers{{id,name,disambiguation,image_path}}}}}}";
-
+        
         public const string PerformerSearchQuery = @"query{{findPerformers({0}){{performers{{id,name,disambiguation,image_path,birthdate}}}}}}";
 
         public const string PerformerQuery = @"query{{findPerformer(id:{0}){{id,name,disambiguation,image_path,alias_list,birthdate,death_date}}}}";
@@ -13,5 +10,10 @@ namespace Stash
         public const string StudiosSearchQuery = @"query{{findStudios({0}){{studios{{id,name,image_path}}}}}}";
 
         public const string StudioQuery = @"query{{findStudio(id:{0}){{id,name,image_path}}}}";
+        
+        public const string SceneSearchQuery = @"query{{findScenes({0}){{scenes{{id,title,date,paths{{screenshot}}movies{{movie{{front_image_path}}}}}}}}}}";
+
+        public const string SceneQuery = @"query{{findScene(id:{0}){{id,title,details,date,paths{{screenshot}}movies{{movie{{front_image_path,director}}}}studio{{name,parent_studio{{name}}}}tags{{name}}performers{{id,name,disambiguation,image_path}}}}}}";
+
     }
 }
