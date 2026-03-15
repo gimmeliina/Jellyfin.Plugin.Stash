20ba273056efb89b05e4834b08e836707f3bf31a
gimmeliina <96843881+gimmeliina@users.noreply.github.com>
2026-03-15 17:23:57 +0200

    Remove blank line in StashAPI.cs
    Remove unnecessary blank line before screenImageUrl check.

--- Jellyfin.Plugin.Stash/Providers/StashAPI.cs
+++ Jellyfin.Plugin.Stash/Providers/StashAPI.cs
@@ -213,7 +213,6 @@ namespace Stash.Providers
                     Url = groupImageUrl,
                 });
             }
-            
             if (!string.IsNullOrWhiteSpace(screenImageUrl))
             {
                 result.Add(new RemoteImageInfo
