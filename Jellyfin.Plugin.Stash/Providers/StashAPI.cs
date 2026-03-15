e391dad748259e2aca848b5e39f6e262d9d5bda7
gimmeliina <96843881+gimmeliina@users.noreply.github.com>
2026-03-15 17:27:28 +0200

    Add check for non-empty screenImageUrl

--- Jellyfin.Plugin.Stash/Providers/StashAPI.cs
+++ Jellyfin.Plugin.Stash/Providers/StashAPI.cs
@@ -213,6 +213,7 @@ namespace Stash.Providers
                     Url = groupImageUrl,
                 });
             }
+            
             if (!string.IsNullOrWhiteSpace(screenImageUrl))
             {
                 result.Add(new RemoteImageInfo
